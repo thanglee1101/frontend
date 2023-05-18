b0a8a0d2f18060303865821123d2680d07732f4f
thanglee1101 <88480074+thanglee1101@users.noreply.github.com>
2023-05-19 00:15:33 +0700

    Initial commit

---
+++ src/qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"64665c55dad7580021a3a611",
+				"64665c55dad7580021a3a614"
+			]
+		},
+		"64665c55dad7580021a3a611": {
+			"id": "64665c55dad7580021a3a611",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"64665c55dad7580021a3a614": {
+			"id": "64665c55dad7580021a3a614",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
